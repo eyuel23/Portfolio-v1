6488c8ecfbfa663d3433c31c6b0e164ab7a33db7
eyuel gebreselassie <eyuelmulugeta05@gmail.com>
2022-12-29 00:03:52 -0500

    added loader component

---
+++ components/Loader/index.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+export default function Loader() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading();
+    }, [3000]);
+    console.log("done");
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <motion.div className="h-screen fixed top-0 bottom-0 left- right-0 w-full bg-primary flex justify-center items-center font-semibold">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+              scale: [1, 2, 2, 1, 1],
+              opacity: "100%",
+            }}
+            transition={{ delay: 0.5, duration: 1 }}
+            className=""
+          >
+            <h1 className="text-6xl text-brandColor">#FREETIGRAY</h1>
+          </motion.div>
+        </motion.div>
+      ) : null}
+    </>
+  );
+}
